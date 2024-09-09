c783c7d6370f9380c6ac71fe0ca3a9c42b07ce5f
Behnoush <behnoush.shahraeini@gmail.com>
2024-09-09 21:14:16 +0330

    3

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/create/',views.post_create, name = 'post_create'),
+    path('post/<int:pk>/edit/',views.post_edit, name = 'post_edit'),
+    path('post/<int:pk>/delete/',views.post_delete, name = 'post_delete'),
+    path('post/<int:pk>/', views.post_comment, name='add_comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,65 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404,redirect
+from .models import Post ,Comment
+from .forms import PostForm , CommentForm
+
+from django.contrib.auth.decorators import login_required
+
 
 def post_list (request):
     posts = Post.objects.all()
     return render(request , 'post_list.html', {'posts':posts})
+
+@login_required
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+           post = form.save(commit=False)
+           post.author = request.user
+           post.save()
+           return redirect('post_list')
+        else:
+            form = PostForm()
+        return render(request, 'blog/post_creat.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk, author=request.user)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save() 
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_delete(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_delete.html', {'post': post})
+
+@login_required
+def post_comment(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.user = request.user
+        comment.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog_app/add_comment.html', {'form': form})
+
+
+
+
+
+
+
+
