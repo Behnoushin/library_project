eecbeb03a63cbe206523df814eac2ad47b75ac17
Behnoush <behnoush.shahraeini@gmail.com>
2024-09-09 14:09:46 +0330

    1

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_list (request):
+    posts = Post.objects.all()
+    return render(request , 'post_list.html', {'posts':posts})
